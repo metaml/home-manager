0a6cfa062372d8311d6e406d23534b1bbe46b138
metaml <metaml@gmail.com>
2024-07-21 22:28:43 -0700

    replaced awscli with awscli2

--- home.nix
+++ home.nix
@@ -18,7 +18,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    awscli
+    awscli2
     bashInteractive
     coreutils
     csvtool
